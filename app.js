114766e977ee7086d9b24ce4f084a8df130852da
Noormdgolam <noormdgolam@gmail.com>
2019-11-08 16:36:13 +0800

    chaged product details

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
-
 app.get("/", function(req, res){
     res.render("main");
 });
@@ -15,7 +13,22 @@ app.get("/superMarket", function(req, res){
     res.render("superMarket");
 });
 app.get("/product1", function(req, res){
-    res.render("product1");
+    res.render("supermarketFlavours/product1");
+});
+app.get("/product2", function (req, res) {
+    res.render("supermarketFlavours/product2");
+});
+app.get("/product3", function (req, res) {
+    res.render("supermarketFlavours/product3");
+});
+app.get("/product4", function (req, res) {
+    res.render("supermarketFlavours/product4");
+});
+app.get("/product5", function (req, res) {
+    res.render("supermarketFlavours/product5");
+});
+app.get("/product6", function (req, res) {
+    res.render("supermarketFlavours/product6");
 });
 let port = process.env.PORT;
 if (port == null || port == "") {
