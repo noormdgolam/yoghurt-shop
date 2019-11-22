c97ce6ac26227d073aa0f829cdad50cbafbcb03c
Noormdgolam <noormdgolam@gmail.com>
2019-11-22 22:19:07 +0800

    created login method with backend

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+//jshint esversion:8
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const session = require("express-session");
+const flash = require("express-flash");
+const override = require("method-override");
 const app = express();
+const users = [];
 // const mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -8,7 +18,82 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("main");
 });
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+);
 
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    save: false,
+    saveUninitialized: false
+  })
+);
+app.use(override("_method"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+app.get("/register", canNotGoBackIfLoggedIn, (req, res) => {
+  res.render("register");
+});
+app.get("/login", canNotGoBackIfLoggedIn, (req, res) => {
+  res.render("login");
+});
+
+app.get("/user", canNotGoToUserWithOutLogIn, (req, res) => {
+  res.render("user", { users: users });
+});
+app.post(
+  "/login",
+  canNotGoBackIfLoggedIn,
+  passport.authenticate("local", {
+    successRedirect: "/user",
+    failureRedirect: "/login",
+    failureFlash: true,
+    successFlash: true
+  })
+);
+app.post("/register", canNotGoBackIfLoggedIn, async (req, res) => {
+  try {
+    const hashPass = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hashPass
+    });
+    res.redirect("/login");
+  } catch (error) {
+    res.redirect("/register");
+  }
+});
+
+function canNotGoToUserWithOutLogIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function canNotGoBackIfLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/user");
+  }
+  next();
+}
+/*-------------------------------------------*/
+
+/*-----------------------------------------*/ 
 app.get("/superMarket", function(req, res){
     res.render("superMarket");
 });
