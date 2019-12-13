63f5ffa3563952a673f9ce7436b9c48458f1fe25
Noormdgolam <noormdgolam@gmail.com>
2019-12-13 15:53:04 +0800

    new update

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,25 @@ router.get('/user', ensureAuthenticated, (req, res) =>
     user: req.user
   })
 );
+
+router.get("/edit", ensureAuthenticated, (req, res) => res.render("edit", { user: req.user }));
+router.get("/setting", ensureAuthenticated, (req, res) => res.render("setting", { user: req.user }));
+
+router.post("/edit", ensureAuthenticated, (req, res) => {
+  const { name, email } = req.body;
+  const id = { _id: req.body.id };
+  const updateItem = { $set: { name: name, email: email } };
+  User.updateOne(id, updateItem, (err) => {
+    if (err) {
+      console.log(err);
+
+    } else {
+      res.redirect("/setting");
+      console.log("Settings updated");
+    }
+  });
+});
+
 router.post("/delete", (req, res) => {
   const deleteItem = req.body.id;
   User.findByIdAndDelete(deleteItem, (err) => {
